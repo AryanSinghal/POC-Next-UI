5a3586129314cc126860c95661fd32fa6797ba27
AryanSinghal <aryan.singhal@successive.tech>
2022-08-15 10:42:14 +0530

    initial commit

---
+++ pages/product/[productId].js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+
+function Product() {
+  const router = useRouter()
+  const { productId } = router.query
+  return <div>Product: {productId}</div>
+}
+
+export default Product;
