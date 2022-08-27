9e8f1223fc5d03fdf3caebd78492a1efffd3694a
AryanSinghal <aryan.singhal@successive.tech>
2022-08-27 08:21:54 +0530

    add static page

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,5 +1,19 @@
-function Products() {
-	return <div>PRODUCT LIST</div>
+export default function Product({ allPokemons }) {
+	return (
+		<ul>
+			{allPokemons.map((poke) => (
+				<li key={poke.url}>{poke.name}</li>
+			))}
+		</ul>
+	);
 }
 
-export default Products;
+export async function getStaticProps() {
+	const response = await fetch(
+		'https://pokeapi.co/api/v2/pokemon/');
+
+	const data = await response.json();
+	return {
+		props: { allPokemons: data.results },
+	};
+}
